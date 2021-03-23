a7d3b28339fcc4f6067e3432584eadd38bd071c5
Jim Richter <jim-x@web.de>
2021-03-23 14:47:36 +0100

    FIRST

---
+++ src/routes/index.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+	/**
+	 * @param {import('@sveltejs/kit).LoadOptions} options
+	 * @returns {import('@sveltejs/kit').Loaded}
+	 */
+	export async function load({ fetch }) {
+		const res = await fetch("api/test");
+
+		if (res.ok) {
+			return {
+				props: {
+					test: await res.json(),
+				},
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load /api/test`),
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let test;
+</script>
+
+{JSON.stringify(test)}
